94168bb2195a6d2603c848e452c2a4555a501b36
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-04-06 18:27:52 +0200

    Create gitignore file Add app terminal

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,1 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+Console.ReadKey();
