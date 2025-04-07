52f86b76e75b9a1e01638b28e6dd3e858b4a5210
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-04-07 19:30:20 +0200

    Introduce user input

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -3,4 +3,10 @@ Console.WriteLine("[S]ee all TODOs");
 Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine("User input: " + userChoice);
+
+
+
 Console.ReadKey();
