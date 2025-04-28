5a3bd81fc1201d3f47ef16b625f30e90291a2d0d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-04-28 22:33:51 +0200

    switch expressions

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -4,9 +4,30 @@ Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
-string userChoice = Console.ReadLine();
-Console.WriteLine("User input: " + userChoice);
+string userChoice = Console.ReadLine().Trim().ToLower();
 
+switch(userChoice)
+{
+    case "s":
+        PrintSelectedOption("See all TODOs");
+        break;
+    case "a":
+        PrintSelectedOption("Add a TODO");
+        break;
+    case "r":
+        PrintSelectedOption("Remove a TODO");
+        break;
+    case "e":
+        PrintSelectedOption("Exit");
+        break;
+    default:
+        PrintSelectedOption("Invalid choice");
+        break;
+}
 
+void PrintSelectedOption(string option)
+{
+    Console.WriteLine(option);
+}
 
 Console.ReadKey();
