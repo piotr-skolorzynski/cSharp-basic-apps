a0e60063dc999a9af4ea7ccd55690129735f94c0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-02-01 12:43:32 +0100

    Add CSharpier code formatter for VSC Modify ToDoList project Develop AddTodo method

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,33 +1,66 @@
-﻿Console.WriteLine("Hello");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿var todos = new List<string>();
 
-string userChoice = Console.ReadLine().Trim().ToLower();
+Console.WriteLine("Hello");
 
-switch(userChoice)
+bool shallExit = false;
+while (!shallExit)
 {
-    case "s":
-        PrintSelectedOption("See all TODOs");
-        break;
-    case "a":
-        PrintSelectedOption("Add a TODO");
-        break;
-    case "r":
-        PrintSelectedOption("Remove a TODO");
-        break;
-    case "e":
-        PrintSelectedOption("Exit");
-        break;
-    default:
-        PrintSelectedOption("Invalid choice");
-        break;
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    string userChoice = Console.ReadLine().Trim().ToLower();
+
+    switch (userChoice)
+    {
+        case "s":
+            PrintSelectedOption("See all TODOs");
+            break;
+        case "a":
+            PrintSelectedOption("Add a TODO");
+            AddTodo();
+            break;
+        case "r":
+            PrintSelectedOption("Remove a TODO");
+            break;
+        case "e":
+            PrintSelectedOption("Exit");
+            shallExit = true;
+            break;
+        default:
+            PrintSelectedOption("Invalid choice");
+            break;
+    }
 }
 
 void PrintSelectedOption(string option)
 {
     Console.WriteLine(option);
+}
+
+void AddTodo()
+{
+    bool isValidDescription = false;
+    while (!isValidDescription)
+    {
+        Console.WriteLine("Enter the TODO description:");
+        var description = Console.ReadLine();
+        if (description == "")
+        {
+            Console.WriteLine("Description cannot be empty.");
+        }
+        else if (todos.Contains(description))
+        {
+            Console.WriteLine("This description must be unique.");
+        }
+        else
+        {
+            isValidDescription = true;
+            todos.Add(description);
+        }
+    }
 }
 
 Console.ReadKey();
