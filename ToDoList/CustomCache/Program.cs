168b4c76c12216bdb902bc7e68d296937beea13c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-02-15 18:30:15 +0100

    Create Cache class Use cache in SlowDataDownloader

--- ToDoList/CustomCache/Program.cs
+++ ToDoList/CustomCache/Program.cs
@@ -10,6 +10,20 @@ Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
+public class Cache<TKey, TData>
+{
+    private readonly Dictionary<TKey, TData> _cachedData = new();
+
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+    {
+        if (!_cachedData.ContainsKey(key))
+        {
+            _cachedData[key] = getForTheFirstTime(key);
+        }
+        return _cachedData[key];
+    }
+}
+
 public interface IDataDownloader
 {
     string DownloadData(string resourceId);
@@ -17,7 +31,14 @@ public interface IDataDownloader
 
 public class SlowDataDownloader : IDataDownloader
 {
+    private readonly Cache<string, string> _cache = new();
+
     public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, DownloadDataWithoutCaching);
+    }
+
+    private string DownloadDataWithoutCaching(string resourceId)
     {
         //let's imagine this method downloads real data,
         //and it does it slowly
