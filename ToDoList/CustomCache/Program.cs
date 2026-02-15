b455b3269241e0a4ee66613aa657a10f48bc973b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-02-15 19:09:18 +0100

    Create another decorator PrintingDataDownloader Compose decorators

--- ToDoList/CustomCache/Program.cs
+++ ToDoList/CustomCache/Program.cs
@@ -1,4 +1,6 @@
-﻿IDataDownloader dataDownloader = new CachingDataDownloader(new SlowDataDownloader());
+﻿IDataDownloader dataDownloader = new CachingDataDownloader(
+    new PrintingDataDownloader(new SlowDataDownloader())
+);
 
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -53,5 +55,22 @@ public class CachingDataDownloader : IDataDownloader
     public string DownloadData(string resourceId)
     {
         return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+
+public class PrintingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+
+    public PrintingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        var data = _dataDownloader.DownloadData(resourceId);
+        Console.WriteLine("Data is ready!");
+        return data;
     }
 }
