6efef28889e6c882e2589ae176eaee3b17384ab2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-02-15 10:34:18 +0100

    Create StrategyExample project Implement an example Simple App displaying Even Odd or Positive numbers

---
+++ ToDoList/StrategyExample/Program.cs
@@ -0,0 +1,76 @@
+﻿var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+
+Console.WriteLine(
+    @"Select filter:
+Even
+Odd
+Positive
+"
+);
+var input = Console.ReadLine();
+List<int> result;
+switch (input)
+{
+    case "Even":
+        result = SelectEven(numbers);
+        break;
+    case "Odd":
+        result = SelectOdd(numbers);
+        break;
+    case "Positive":
+        result = SelectPositive(numbers);
+        break;
+    default:
+        throw new NotSupportedException($"{input} is not a valid filter");
+}
+
+Print(result);
+
+void Print(List<int> numbers)
+{
+    Console.WriteLine(string.Join(", ", numbers));
+}
+
+List<int> SelectEven(List<int> numbers)
+{
+    var result = new List<int>();
+    foreach (var number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+}
+
+List<int> SelectOdd(List<int> numbers)
+{
+    var result = new List<int>();
+    foreach (var number in numbers)
+    {
+        if (number % 2 == 1)
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+}
+
+List<int> SelectPositive(List<int> numbers)
+{
+    var result = new List<int>();
+    foreach (var number in numbers)
+    {
+        if (number > 0)
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+}
+
+Console.ReadKey();
