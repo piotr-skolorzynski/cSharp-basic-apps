14c53ab060b52366c8e80cd610bf4539016b1b3a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-02-20 10:20:14 +0100

    Finish the app Show planet stats using MaxBy and MinBy linq methods

--- ToDoList/StarWarsPlanetsStats/App/StarWarsPlanetsStatsApp.cs
+++ ToDoList/StarWarsPlanetsStats/App/StarWarsPlanetsStatsApp.cs
@@ -43,8 +43,51 @@ public class StarWarsPlanetStatsApp
 
         foreach (var planet in planets)
         {
-            System.Console.WriteLine(planet);
+            Console.WriteLine(planet);
         }
+
+        Console.WriteLine();
+
+        Console.WriteLine("The statistics of which property would you like to see?");
+        Console.WriteLine("population");
+        Console.WriteLine("diameter");
+        Console.WriteLine("surface water");
+
+        var userChoice = Console.ReadLine();
+
+        if (userChoice == "population")
+        {
+            ShowStatistics(planets, "population", planet => planet.Population);
+        }
+        else if (userChoice == "diameter")
+        {
+            ShowStatistics(planets, "diameter", planet => planet.Diameter);
+        }
+        else if (userChoice == "seurface water")
+        {
+            ShowStatistics(planets, "surface water", planet => planet.SurfaceWater);
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice!");
+        }
+    }
+
+    private void ShowStatistics(
+        IEnumerable<Planet> planets,
+        string propertyName,
+        Func<Planet, int?> propertySelector
+    )
+    {
+        var planetWithMaxPropertyName = planets.MaxBy(propertySelector);
+        Console.WriteLine(
+            $"Max {propertyName} is: {propertySelector(planetWithMaxPropertyName)}. Planet: {planetWithMaxPropertyName.Name}"
+        );
+
+        var planetWithMinPropertyName = planets.MinBy(propertySelector);
+        Console.WriteLine(
+            $"Min {propertyName} is: {propertySelector(planetWithMinPropertyName)}. Planet: {planetWithMinPropertyName.Name}"
+        );
     }
 
     private IEnumerable<Planet> ToPlanets(Root? root)
